4ac12c97ac014e64e20547758ad6d543408edc42
Moshood Owolabi <98819100+Msdot001@users.noreply.github.com>
2022-07-11 12:19:00 +0200

    Create Note.sql
    Add Database Relationship

---
+++ PostgreSQL/Note.sql
@@ -0,0 +1,25 @@
+Database Relationships
+
+- One-to-One Relationships
+- One-to-Many Relationships
+- Many-to-Many Relationships
+
+One-to-One Relationship: 
+In this kind of relationship, Primary key value can appear a maximum of once or none in the foreign key column.
+This kind of relationship is kind of rare
+The Parent table always contain the Primary key while the Child Table always conatin the Foreign Keys
+
+
+One-to-Many Relationships
+This is the most common type of data relationship
+The Primary key values can appear multiple time in Foreign key column
+E.g Intergram user and instagram Photo; An instagram user (one) can have a many photo on thise account. 
+
+
+Many-to-Many Relationships
+This occur when two tables can have many instances of each other.
+E.g Authors and Books; A author can write many books and a book canhave many authors
+    Authou and Movies; Actor can perform in many Movies and a Movies can contai many Actors
+
+for this can relationship, primary key and foreign is not used to linked table rather we need Junction table 
+Junction table contain the foreign key of the two table to be linked together.
