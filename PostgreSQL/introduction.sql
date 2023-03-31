14436343dbda304e054ee065dc9d101ef666f84f
Moshood Owolabi <98819100+Msdot001@users.noreply.github.com>
2023-03-31 17:09:55 +0200

    Introduction to SQL

---
+++ PostgreSQL/introduction.sql
@@ -0,0 +1,15 @@
+SQL is a collection of orgainised information or data that is stored in a table for which are access by DBMS
+
+DBMS also called Data System Management System  are software system that allow user to store, organise and manage 
+data in the database.
+
+Example of DBMS include PostgreSQL, MySQL, Microsoft SQL Serve and Oracle. 
+
+DBMS provide wide range of service such as Data Storage, Data Retrieval, Data Security, Data integrity and Data backup and recovery.
+
+PostgreSQL
+- It an relational database management system. (relational because data is store in table(role and column). 
+- Each column has specific unique Primary key 
+- Table are linked together with Foreign Key 
+
+
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # SQL_Sample
-A collection of my sample SQL files
+A collection of my sample SQL files. I was able to learn and practices using SQl language to perform activites which can be grouped into;
+
+-SQL Basics:
+- SQL Aggregations
+- SQL joins
+- Advance SQL
+
+The SQL Basics includes;
+
+- Create Tables
+- Create Column
+- Insert into Columns
+- Select Statement
+- Clause & Keywords used with SElECT statement
+
+
